c643b1517212259dc2a2a46c2fe82f62ca64c04f
Aditya Palve <71967068+ThanoS-max@users.noreply.github.com>
2021-01-11 22:21:02 +0530

    Add files via upload

---
+++ main.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace MyApp
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello MLH Local Hack Day: Build");
+      Console.WriteLine("Y'all are the best <3");
+      // Console.WriteLine("BTW, I am Aditya. Nice to meet you all!");
+      var name = "Aditya. no no not Aditya OP-rioe XD, but Aditya Palve. Nice to meet you all!";
+      Console.WriteLine($"I am {name}");
+    }
+  }
+}
